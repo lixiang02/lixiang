e7f99567f43e13cce8c5fbef5e37e5fc332966d1
lixiang <87375415@qq.com>
2019-01-09 18:21:49 +0800

    second push

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const main = async () => {
-    console.log('===sucessful===start===service====');
+    console.log('===sucessful===start===service==11111==');
 };
 
 main().catch(err => err);
